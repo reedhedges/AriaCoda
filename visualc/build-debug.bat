b2f323ce69c0947ef77ff3d55250dd99896fa194
Reed Hedges <reed@interreality.org>
2021-03-21 07:55:48 -0700

    Fix "badges" in README.md [travis skip]

--- visualc/build-debug.bat
+++ visualc/build-debug.bat
@@ -1,5 +1,5 @@
 @echo off
-REM run with the MSVC Developer Command Prompt like this:
-REM C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevComd.bat build.bat
+REM run from the MSVC Developer Command Prompt or similar to:
+REM C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat build.bat
 
 MSBuild.exe AriaCoda.sln -property:Configuration=Debug -maxCpuCount 
