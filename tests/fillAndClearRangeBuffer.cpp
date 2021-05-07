168f11bc581a0838ee60cec8f665db93f535e0a8
Reed Hedges <reed@interreality.org>
2021-05-07 17:17:37 -0400

    Add basicRangeDeviceTest to log artifical range device data. Change fillAndClearRangeBuffer test to invalidate readings every 5 seconds rather than just clear the buffer.

--- tests/fillAndClearRangeBuffer.cpp
+++ tests/fillAndClearRangeBuffer.cpp
@@ -5,11 +5,18 @@
 
 // use this to test ArRangeBuffer data management performance.
 // this simulates a range sensor that provides lots of data 
-// which will be stored in an unusually large buffer.
+// which will be stored in a buffer of 1000 items (which is a bit larger than normal, but should still result in the buffer filling
+// at items needing to be re-used).  After every 5000 items are added, every other item in the buffer is invalidated.
 
-// Before changing ArRangeBuffer to store ArPoseWithTime directly (it stored allocated ArPoseWithTime objects), 
-// Reed tested this to be about 4 seconds, with a range buffer size of 1000, storing 200,000 random points 
-// (x=(0,30000), y=(0,30000)) omitting near distance of 100mm, and clearing out the buffer every 5000 points added.
+// Before changing ArRangeBuffer to store list of ArPoseWithTime objects with
+// simple list pointer changes to add/reuse/reserve (via splice())  it stored a
+// list of pointers to allocated ArPoseWithTime objects, which were
+// allocated/deallocated to add/delete and pointers were copied to reuse and
+// reserve items.  I timed this test with the old method to be about 4 seconds, 
+// with a range buffer size of 1000, storing 200,000 random points 
+// (x in range (0,30000), y in range (0,30000)) omitting near distance of 10,000mm (so it shuold have to search the whole list every time), and clearing out 
+// the buffer every 5000 points added.
+// I timed this with the new method about 900 ms.
 
 int main()
 {
@@ -23,11 +30,20 @@ int main()
     // do periodic clearing of buffer
     if(i % 5000 == 0)
     {
-      rangebuffer.clear();
+      //rangebuffer.clear();
       //printf("%6u  clear     \r", i);
       //fflush(stdout);
+
+      rangebuffer.beginInvalidationSweep();
+      for (auto i = rangebuffer.getBegin(); i != rangebuffer.getEnd(); )
+      {
+        rangebuffer.invalidateReading(i);
+        if( (++i) == rangebuffer.getEnd())
+          break;
+        ++i;
+      }
     }
   }
+  //rangebuffer.logData(ArLog::Verbose, "", "none", "test");
   printf("\nTime=%ld ms\n", t.mSecSince());
-
 }
