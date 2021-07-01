beb6d270af1ef84e3b9de6102258c56e6a42124f
Reed Hedges <reed@interreality.org>
2021-07-01 17:28:18 -0400

    Re-add assertion to ArSyncTask that task functor time was nonnegative (but corrected from previous assertion which was only >0)

--- src/ArSyncTask.cpp
+++ src/ArSyncTask.cpp
@@ -265,6 +265,7 @@ AREXPORT void ArSyncTask::run()
   if (myFunctor != NULL)
     myFunctor->invoke();
   const long took = runTime.mSecSince();
+  assert(took >= 0);
   long warningTime = 0;
   if (myNoTimeWarningCB != NULL && !myNoTimeWarningCB->invokeR() && 
       myFunctor != NULL && myWarningTimeCB != NULL &&
