0b44639ac24531d9029fca1de84f5d33549d08c6
Reed Hedges <reed@interreality.org>
2022-12-01 10:47:28 -0500

    Mark variables in ArSimUtil as unused to prevent warnings

--- include/Aria/ArSimUtil.h
+++ include/Aria/ArSimUtil.h
@@ -116,8 +116,8 @@ protected:
     if(pkt->getID() != 0x62) return false;
     lock();
     myLastPacketTimeReceived.setToNow();
-    char v = pkt->bufToByte();
-    v = pkt->bufToByte();
+    [[maybe_unused]] char ignore1 = pkt->bufToByte();
+    [[maybe_unused]] char ignore2 = pkt->bufToByte();
     uint32_t flags = pkt->bufToUByte4();
     myHaveGPS = flags & ArUtil::BIT1;
     myHaveOdomError = flags & ArUtil::BIT2;
